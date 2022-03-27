2fa9fb00b7cae6b4d20e7b34078066e26168801d
Кирилл Логинов <thekiralog@gmail.com>
2022-03-27 20:15:02 +0400

    16 omw

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+print("Введите первую точку")
+x1 = float(input('X: '))
+y1 = float(input('Y: '))
+print("\nВведите вторую точку")
+x2 = float(input('X: '))
+y2 = float(input('Y: '))
+
+x_diff = x2 - x1
+y_diff = y2 - y1
+k = y_diff / x_diff
+b = y2 - k * x2
+
+print("Уравнение прямой, проходящей через эти точки:")
+print("y = ", k, " * x + ", b)
--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+friends_quantity = int(input('Количество друзей: '))
+debts_quantity = int(input('Долговых расписок: '))
+friends = the_dict = {friend: 0 for friend in range(1, friends_quantity + 1)}
+for i in range(debts_quantity):
+    print('\n', i + 1, ' расписка')
+    to_friend = int(input('Кому: '))
+    from_friend = int(input('От кого: '))
+    equity = int(input('Сколько: '))
+    friends[to_friend] -= equity
+    friends[from_friend] += equity
+print('\nБаланс друзей:')
+for key, value in friends.items():
+    print(key, ':', value)
