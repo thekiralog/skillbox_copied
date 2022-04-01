3438e88e0987e858b61dda75dd43c6de0c2a26d7
Кирилл Логинов <thekiralog@gmail.com>
2022-04-01 18:44:46 +0400

    16.10 done

--- Module16/main.py
+++ Module16/main.py
@@ -1,23 +1,23 @@
 import random
 
 
-def numeric_palindrome(desired: list,):
-    for index, value in enumerate(desired[::-1]):
-        if index > -1 or desired[value] != desired[index - 1]:
-            index -= 1
-            numeric_palindrome(desired[index::-1])
-        elif index > -1 and desired[value] == desired[index - 1]:
-            index -= 1
-            numeric_palindrome(desired[index::-1])
-        else:
-            return len(desired[:index])
+def numeric_palindrome(desired: list, ):
+    reverse_desired = desired[::-1]
+    if desired == reverse_desired:
+        return True
+    else:
+        return False
 
 
-number = [random.randint(1, 9) for _ in range(random.randint(4, 5))]
-print(number)
-print(numeric_palindrome(number))
-print(number[:numeric_palindrome(number)])
+numbers = [random.randint(0, 9) for _ in range(random.randint(4, 10))]
+answer = list()
+for i_num in range(0, len(numbers)):
+    if numeric_palindrome(numbers[i_num:]):
+        answer = numbers[:i_num]
+        break
+answer.reverse()
 
-
-
+print('Исходный список', numbers)
+print('Нужно чисел для палиндрома', len(answer))
+print('Эти числа:', answer)
 
