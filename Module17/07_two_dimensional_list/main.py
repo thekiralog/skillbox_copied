764da50d8a8cf3c0a8608c1dfa720ef704f61185
folky <thekiralog@gmail.com>
2022-04-06 09:20:32 +0400

    17.7 fixed

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,3 +1,5 @@
 import random
-charterer_task = [[random.randint(0, 9) for _ in range(random.randint(0, 9))] for __ in range(random.randint(0, 9))]
+charterer_task = [[[random.randint(0, 9) for lower_list in range(5)]
+                  for middle_list in range(3)]
+                  for top_list in range(1)]
 print(charterer_task)
