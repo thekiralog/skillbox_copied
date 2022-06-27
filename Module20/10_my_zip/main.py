16eae897e65823696aaa25e5c63b63b6647e584a
Folky <thekiralog@gmail.com>
2022-06-27 19:06:14 +0400

    20 fixed

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,9 +1,11 @@
 def my_zip(iter_1, iter_2):
-    for action in range(min(len(iter_1), len(iter_2))):
-        print((iter_1[action], iter_2[action]))
+    result = ((iter_1[i_elem], iter_2[i_elem])
+              for i_elem in range(min(len(iter_1), len(iter_2))))
+    return result
 
 
 user_str = input('Строка: ')
 user_tpl = tuple(input('Кортеж чисел (через пробел): ').split())
-print(zip(user_str, user_tpl))
-my_zip(user_str, user_tpl)
+print(my_zip(user_str, user_tpl))
+for i_item in my_zip(user_str, user_tpl):
+    print(i_item)
