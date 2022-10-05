07d8d96f5de4f83b6f1aab95980f7092822c162d
Folky <thekiralog@gmail.com>
2022-10-05 18:13:24 +0400

    22 module 5/9

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+import os
+
+
+def catalog_content(current_path, content=[]):
+    for i_elem in os.listdir(current_path):
+        path = os.path.join(current_path, i_elem)
+        if os.path.isfile(path):
+            content.append(path)
+        if os.path.isdir(path):
+            content.append(path)
+            catalog_content(path, content)
+    return content
+
+
+user_path = input('Введите путь: ')
+common_file_size = 0
+sub_folders_quantity = 0
+files_quantity = 0
+
+for i_path in catalog_content(user_path):
+    if os.path.isfile(i_path):
+        common_file_size += os.path.getsize(i_path)
+        files_quantity += 1
+    elif os.path.isdir(i_path):
+        sub_folders_quantity += 1
+
+print(
+    '{input_path}\n'
+    'Размер каталога (в Кб): {common_size}\n'
+    'Количество подкаталогов: {sub_folders}\n'
+    'Количество файлов: {files_quantity}'.format(
+        input_path=user_path,
+        common_size=common_file_size / 1024,
+        sub_folders=sub_folders_quantity,
+        files_quantity=files_quantity
+    )
+)
--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+import os
+import string
+
+
+def is_file_exist(path):
+    if os.path.exists(path):
+        return True
+    else:
+        return False
+
+
+user_string = input('Введите строку: ')
+local_disks = list()
+for i_letter in string.ascii_uppercase:
+    i_disk = i_letter + ':\\'
+    if os.path.isdir(i_disk):
+        local_disks.append(i_disk)
+print('Введите номер диска на котором хотите сохранить файл:', str(local_disks)[1:-1])
+user_disk = local_disks[int(input())-1]
+user_path = input('Куда хотите сохранить документ? Введите последовательность папок (через пробел):').split()
+file_path = str()
+for i_folder in user_path:
+    file_path += i_folder + '\\'
+file_path = os.path.join(user_disk, file_path)
+file_name = input('Введите имя файла: ') + '.txt'
+file_path = os.path.join(file_path, file_name)
+file = open(file_path, 'w', encoding='utf-8')
+if is_file_exist(file_path):
+    user_rewrite = input('Вы действительно хотите перезаписать файл? ').lower()
+    if user_rewrite == 'да':
+        print('Файл успешно перезаписан!')
+        file.write(user_string)
+    else:
+        exit()
+else:
+    print('Файл успешно сохранен!')
+    file.write(user_string)
+file.close()
